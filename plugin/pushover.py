ac68ca806401f8d1635f6e7aac9cbd69c99823a5
Samuel Scherer <github@samuelscherer.de>
2021-11-18 15:46:46 +0100

    created empty plugin file for pushover

---
+++ plugin/pushover.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""!
+    ____  ____  ______       __      __       __       _____
+   / __ )/ __ \/ ___/ |     / /___ _/ /______/ /_     |__  /
+  / __  / / / /\__ \| | /| / / __ `/ __/ ___/ __ \     /_ <
+ / /_/ / /_/ /___/ /| |/ |/ / /_/ / /_/ /__/ / / /   ___/ /
+/_____/\____//____/ |__/|__/\__,_/\__/\___/_/ /_/   /____/
+                German BOS Information Script
+                     by Bastian Schroll
+
+@file:        pushover.py
+@date:        18.11.2021
+@author:      Samuel Scherer
+@description: Pushover Plugin
+"""
+import logging
+from plugin.pluginBase import PluginBase
+
+# ###################### #
+# Custom plugin includes #
+
+# ###################### #
+
+logging.debug("- %s loaded", __name__)
+
+
+class BoswatchPlugin(PluginBase):
+    """!Description of the Plugin"""
+    def __init__(self, config):
+        """!Do not change anything here!"""
+        super().__init__(__name__, config)  # you can access the config class on 'self.config'
+
+    def onLoad(self):
+        """!Called by import of the plugin
+        Remove if not implemented"""
+        pass
+
+    def setup(self):
+        """!Called before alarm
+        Remove if not implemented"""
+        pass
+
+    def fms(self, bwPacket):
+        """!Called on FMS alarm
+
+        @param bwPacket: bwPacket instance
+        Remove if not implemented"""
+        pass
+
+    def pocsag(self, bwPacket):
+        """!Called on POCSAG alarm
+
+        @param bwPacket: bwPacket instance
+        Remove if not implemented"""
+        pass
+
+    def zvei(self, bwPacket):
+        """!Called on ZVEI alarm
+
+        @param bwPacket: bwPacket instance
+        Remove if not implemented"""
+        pass
+
+    def msg(self, bwPacket):
+        """!Called on MSG packet
+
+        @param bwPacket: bwPacket instance
+        Remove if not implemented"""
+        pass
+
+    def teardown(self):
+        """!Called after alarm
+        Remove if not implemented"""
+        pass
+
+    def onUnload(self):
+        """!Called by destruction of the plugin
+        Remove if not implemented"""
+        pass
